814c8427d89fa41109dbad6f5289370b067cdd48
Slimmgrimmith <dannywukasch@gmail.com>
2023-03-18 21:11:06 -0500

    Drills to create constructor & prototypes

--- objects.js
+++ objects.js
@@ -1,15 +1,65 @@
-const myObject = {
-    property: 'String!',
-    property2: 77,
-    "property three": function(){
-
-    }
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
 }
 
-function Book(title, author) {
-    this.title = title;
-    this.author = author;
+Book.prototype.info = function() {
+    return `${this.title}, ${this.author}, ${this.pages} pages, ${this.read}`
 }
 
-const fakeBook = new Book('The Great Gatsby', 'John F. Kennedy');
-console.log(fakeBook.title);
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'have not');
+console.log(theHobbit.info());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const myObject = {
+//     property: 'String!',
+//     property2: 77,
+//     "property three": function(){
+
+//     }
+// }
+
+// function Book(title, author) {
+//     this.title = title;
+//     this.author = author;
+// }
+
+// const fakeBook = new Book('The Great Gatsby', 'John F. Kennedy');
+// console.log(fakeBook.title);
+
+// Book.prototype.sayName = function() {
+//     console.log(this.name);
+// }
+
+// function fictionBook(title) {
+//     this.title = title;
+//     this.author = 'Stephen King';
+// }
+
+// fictionBook.prototype = Object.create(Book.prototype)
+
+// const book = new fictionBook('The Fog');
+// book.sayName();
+// book.author;
